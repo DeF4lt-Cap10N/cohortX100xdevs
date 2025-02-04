1035d4ebec4b4f38ed6544ecd20565fcbd74f066
akash1675 <captainakash1675@gmail.com>
2025-02-05 02:46:41 +0530

    add week-13

---
+++ week-13.1/src/App.jsx
@@ -0,0 +1,16 @@
+
+function App() {
+
+
+  return (
+    <>
+      <div className="md:flex  ">
+        <div className="bg-green-600 md:w-1/3">first</div>
+        <div className="bg-red-600 md:w-1/3">second</div>
+        <div className="bg-pink-600 md:w-1/3">third</div>
+      </div>
+    </>
+  )
+}
+
+export default App
