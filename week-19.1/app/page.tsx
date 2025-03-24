4c81f1d1e703215cbdb91a8d743d5357ff53d3eb
Rahul sharma <captainakash1675@gmail.com>
2025-03-24 22:24:36 +0530

    add week-19

--- week-19.1/app/page.tsx
+++ week-19.1/app/page.tsx
@@ -1,9 +1,21 @@
-import Image from "next/image";
+
+import Link from "next/link";
 
 export default function Home() {
   return (
-  <div>
-    hello
+  <div className="text-lg w-screen h-screen flex justify-center items-center">
+    <div>
+       Todo Application
+       <br />
+       <Link className="text-md border m-2" href="/signin">
+       signin to ToDo application
+       </Link>
+       <br />
+       <Link className="text-md border m-2" href="/signup">
+       signup to ToDo application
+       </Link>
+    </div>
+   
   </div>
   );
 }
---
+++ week-19.1/app/signin/page.tsx
@@ -0,0 +1,20 @@
+"use client"
+
+export default function Signin(){
+
+   return (
+      <div className="w-screen h-screen flex justify-center items-center">
+         <div className="border m-2">
+            <input className="p-2 border m-2" type="text" placeholder="Name"/>
+            <br />
+            <input className="p-2 border m-2" type="text" placeholder="Email"/>
+            <br />
+            <div onClick={()=>{}} className="flex justify-center">
+                <button className="border m-2 p-2 cursor-pointer">Sign In</button>
+            </div>
+           
+         </div>
+
+      </div>
+   )
+}
